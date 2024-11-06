afdaf981f59d7661956d44249dbf2265a8a7151b
petrebagashvili <petre.bagashvili204@ens.tsu.edu.ge>
2024-11-06 06:09:57 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-let Age=17;
+// let Age=17;
+// if (Age >=0 && Age <=12) {
+//     console.log(`Ticket price = $5`);
+// } else if (Age >12 && Age <=17) {
+//     console.log(`Ticket price = $10`);
+// } else if (Age >=18 && Age <=64) {
+//     console.log(`Ticket price = $15`);
+// } else if (Age >64 && Age <=120) {
+//     console.log(`Ticket price = $8`);
+// } else {
+//     console.log(`age is wrong and please correct it`);      
+// }
+
+
+let Age=68;
+let Ticketprice;
 if (Age >=0 && Age <=12) {
-    console.log(`Ticket price = $5`);
+    Ticketprice="$5";
+    console.log(`Ticket price = ${Ticketprice}`);
 } else if (Age >12 && Age <=17) {
-    console.log(`Ticket price = $10`);
+    Ticketprice="$10";
+    console.log(`Ticket price = ${Ticketprice}`);
 } else if (Age >=18 && Age <=64) {
-    console.log(`Ticket price = $15`);
+    Ticketprice="$15";
+    console.log(`Ticket price = ${Ticketprice}`);
 } else if (Age >64 && Age <=120) {
-    console.log(`Ticket price = $8`);
+    Ticketprice="$8";
+    console.log(`Ticket price = ${Ticketprice}`);
 } else {
     console.log(`age is wrong and please correct it`);      
 }
